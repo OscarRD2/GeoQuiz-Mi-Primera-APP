5a46dd37352a3e2f7f594e8ee173a8695dc0d847
Oscar Romero Doblas <oscar.romero@students.salle.url.edu>
2022-02-17 18:09:43 +0100

    Initial commit

---
+++ app/src/main/java/com/androidpprog2/com/MainActivity.java
@@ -0,0 +1,52 @@
+package com.androidpprog2.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //Creacion de los Button's
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Vinculamos los Buttons con la View, referenciandolos con el id
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Ha clicado en el boton de TRUE
+                Toast toast= Toast.makeText(getApplicationContext(),
+                        R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,250);
+                toast.show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Ha clicado en el boton de FALSE
+                Toast toast= Toast.makeText(getApplicationContext(),
+                        R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,250);
+                toast.show();
+            }
+        });
+    }
+
+
+
+
+
+}
